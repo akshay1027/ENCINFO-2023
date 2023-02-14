e987604d7dd08d3f0da1b2aa6082349703774cac
Moginder <mogiegan@gmail.com>
2023-02-14 15:59:57 +0530

    Routes crted and added separate workshop site

---
+++ src/components/googleformtest.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const googleform =()=>{
+    return(
+
+
+        <section className='my-[70px] xl:my-[150px]'>
+        <div className='container mx-auto'>
+          {/* text */}
+          <div className='text-center'>
+          <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-slate-800 md:text-6xl md:tracking-tight">
+              <span>WORKSHOP</span> | <span class="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-blue-700 to-sky-400 lg:inline">BUILD & FLY YOUR OWN DRONE</span>
+            </h1>
+      
+       
+            <ul className="list-outside py-3" data-aos='fade-right'>
+  <li className="py-3 ">Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className="py-3">Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className="py-3">Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className="py-3">Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className="py-3">Now this is a story all about how, my life got flipped-turned upside down</li>
+
+
+  
+</ul>
+<div align="center">
+            <iframe className="w-4/5 h-full ascept-video align-center " src="https://forms.gle/f8gxqtZfZxjnyb158"></iframe></div>
+</div>
+        </div>
+        </section>
+    )
+}
+
+export default googleform;
